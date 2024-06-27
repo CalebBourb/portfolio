9778de780dfd70a32fc7cbe6663a1e392a553d5b
calebbourb <calebbourbonnais1@gmail.com>
2024-06-27 17:31:19 -0600

    yay its starting

---
+++ portfolio-site/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/global.css";
+
+const Home = () => {
+    return (
+        <div className="w-screen h-screen">
+        <h1>Welcome to my portfolio site!</h1>
+        <p>
+            This is a simple portfolio site created using React. Feel free to
+            explore the site and learn more about me.
+        </p>
+        </div>
+    );
+}
+export default Home;

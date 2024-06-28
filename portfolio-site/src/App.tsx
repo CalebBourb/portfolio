510532abbb8938820d38d33e0680d5a7a3bfbd55
calebbourb <calebbourbonnais1@gmail.com>
2024-06-27 20:05:30 -0600

    made some progress pog

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { Routes, Route, useLocation} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );

1c4e01e9c1aeca5a35d2b3ba273594a76fee0677
calebbourb <calebbourbonnais1@gmail.com>
2024-07-15 17:11:19 -0600

    changing routes

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/global.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
@@ -14,17 +14,22 @@ function App() {
       <div className="">
         <div className="h-[4vh] max-sm:h-0">
         </div>
-        <div className="h-[12vh] z-10 items-center w-screen fixed">
-          <Navigation />
-        </div>
+        
         <div className="grid">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/Resume" element={""} />
-          </Routes>
+          <BrowserRouter>
+            <div className="h-[12vh] z-10 items-center w-screen fixed">
+              <Navigation />
+            </div>
+            <div className="pt-20">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/Resume" element={""} />
+              </Routes>
+            </div>
+          </BrowserRouter>
         </div>
       </div>
     </div>
